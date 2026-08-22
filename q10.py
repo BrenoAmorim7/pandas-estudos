450266e82be206bbe263d6d4e9471d4502631f26
BrenoNunesA <309731043+BrenoAmorim7@users.noreply.github.com>
2026-08-21 21:41:46 -0300

    atualização , codigos da prova utilizando bibliotecas

--- q10.py
+++ q10.py
@@ -5,4 +5,4 @@ import matplotlib.pyplot as plt
 url = "https://raw.githubusercontent.com/DATAUNIRIO/Base_de_dados/master/Estados.csv"
 df_dadosbrasil = pd.read_csv(url,sep=";",decimal=",")
 
-print(df_dadosbrasil['Regiao_Geografica'].value_counts())
+print(df_dadosbrasil['Regiao_Geografica'].value_counts()) 
